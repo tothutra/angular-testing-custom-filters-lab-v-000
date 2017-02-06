cc8be94b8d9a1f8024ca959e043a0e39cad8774d
tttra <tothutra@gmail.com>
2017-02-06 01:08:01 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,28 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  beforeEach(inject(function ($filter) {
+    favoriteFood = $filter('favoriteFood');
+  }));
+
+  it('should filter results by food', function () {
+    var list = [
+      {
+        name: 'Test',
+        favoriteFood: 'Bread'
+      },
+      {
+        name: 'Test 2',
+        favoriteFood: 'Rice'
+      },
+      {
+        name: 'Test 3',
+        favoriteFood: 'Rice'
+      }
+    ]
+    var result = $filter('favoriteFood')(list, "Rice")
+    expect(result.length).toBe(2)
+    expect(result[0].name).toBe('Test 2')
+  })
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -3,8 +3,17 @@ describe('removeAllVowels Filter', function () {
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+  }));
+
+  beforeEach(inject(function ($filter) {
+    removeAllVowels = $filter('removeAllVowels');
+  }));
+
+  it("remove all vowels", function () {
+    var results = $filter('removeAllVowels')('TraToDaleWatkins')
+    expect(results).toEqual("TrTDlWtkns")
+  })
 
 });
